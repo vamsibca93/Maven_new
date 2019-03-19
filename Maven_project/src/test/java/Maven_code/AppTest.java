59690459f65605af72a2baec204f3c09be24945d
vamsidevops <vamsilike9@gmail.com>
2019-03-20 00:34:02 +0530

    Maven build integration

---
+++ Maven_project/src/test/java/Maven_code/AppTest.java
@@ -0,0 +1,20 @@
+package Maven_code;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
